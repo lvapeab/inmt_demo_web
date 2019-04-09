6bb2264414429c6e5748dce08f8a116fa342a95a
lvapeab <lvapeab@gmail.com>
2019-04-09 17:12:16 +0200

    Joint modes

--- demo-web/inmt_sampler.php
+++ demo-web/inmt_sampler.php
@@ -12,7 +12,7 @@ function file_get_contents_utf8($fn) {
 
 if (flock($fp, LOCK_EX))
 {
-	$vid_idx=$_GET['vid_idx'];
+	$source=$_GET['source'];
 	$prefix=$_GET['prefix'];
 	$learn=$_GET['learn'];
 	$beam_size=$_GET['beam_size'];
@@ -20,7 +20,7 @@ if (flock($fp, LOCK_EX))
 	$coverage_norm=$_GET['coverage_norm'];
 	$alpha_norm=$_GET['alpha_norm'];
 
-	$url = '158.42.161.45:6543/?vid_idx='.urlencode($vid_idx).'&prefix='.urlencode($prefix).'&learn='.urlencode($learn).'&beam_size='.urlencode($beam_size).'&length_norm='.urlencode($length_norm).'&coverage_norm='.urlencode($coverage_norm).'&alpha_norm='.urlencode($alpha_norm);
+	$url = '127.0.0.1:6542/?source='.urlencode($source).'&prefix='.urlencode($prefix).'&learn='.urlencode($learn).'&beam_size='.urlencode($beam_size).'&length_norm='.urlencode($length_norm).'&coverage_norm='.urlencode($coverage_norm).'&alpha_norm='.urlencode($alpha_norm);
     $ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, -1);
--- demo-web/inmt_sampler_video.php
+++ demo-web/inmt_sampler_video.php
@@ -9,20 +9,24 @@ function file_get_contents_utf8($fn) {
           mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
 }
 
+
 if (flock($fp, LOCK_EX))
 {
-	$source=$_GET['source'];
+	$vid_idx=$_GET['vid_idx'];
+	$prefix=$_GET['prefix'];
+	$learn=$_GET['learn'];
 	$beam_size=$_GET['beam_size'];
 	$length_norm=$_GET['length_norm'];
 	$coverage_norm=$_GET['coverage_norm'];
 	$alpha_norm=$_GET['alpha_norm'];
-	$url = '127.0.0.1:6542/?source='.urlencode($source).'&beam_size='.urlencode($beam_size).'&length_norm='.urlencode($length_norm).'&coverage_norm='.urlencode($coverage_norm).'&alpha_norm='.urlencode($alpha_norm);
-	$ch = curl_init();
+
+	$url = '158.42.161.45:6543/?vid_idx='.urlencode($vid_idx).'&prefix='.urlencode($prefix).'&learn='.urlencode($learn).'&beam_size='.urlencode($beam_size).'&length_norm='.urlencode($length_norm).'&coverage_norm='.urlencode($coverage_norm).'&alpha_norm='.urlencode($alpha_norm);
+    $ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, -1);
 	$out = curl_exec($ch);
 	curl_close($ch);
-	echo $out;
+       	echo $out;
 }
 else
 {
